505b9058d7a6fc8d5b17347ab8f960f4144444cf
Ryno091510 <ryan.g.mejeur@gmail.com>
2026-02-11 19:50:28 -0500

    Added Javadocs to the NetworkedConfig file

--- src/main/java/frc/robot/networking/NetworkedConfig.java
+++ src/main/java/frc/robot/networking/NetworkedConfig.java
@@ -68,6 +68,9 @@ public class NetworkedConfig {
             .getStringTopic("Debug Field Zone").getEntry("");
         
 
+        /**
+         * Initializes the NetworkTables entries for the turret subsystem.
+         */
         public static void initializeDefaults() {
             // Entries have defaults declared already, simply verify they are created on initialization
             shooterKP.get();
@@ -86,77 +89,97 @@ public class NetworkedConfig {
         }
         
         // Launcher
+        
+        /** Gets the kP for the shooter's PID. */
         public static double getShooterKP() {
             return shooterKP.get();
         }
         
+        /** Gets the kI for the shooter's PID. */
         public static double getShooterKI() {
             return shooterKI.get();
         }
         
+        /** Gets the kD for the shooter's PID. */
         public static double getShooterKD() {
             return shooterKD.get();
         }
         
+        /** Gets the kV for the shooter's Feed-Forward. */
         public static double getShooterKV() {
             return shooterKV.get();
         }
         
+        /** Gets the target RPM for the shooter. */
         public static double getTargetRPM() {
             return targetRPM.get();
         }
         
         // Hood
+        /** Gets the kP for the hood's PID. */
         public static double getHoodKP() {
             return hoodKP.get();
         }
         
+        /** Gets the kI for the hood's PID. */
         public static double getHoodKI() {
             return hoodKI.get();
         }
         
+        /** Gets the kD for the hood's PID. */
         public static double getHoodKD() {
             return hoodKD.get();
         }
         
+        /** Gets the target angle for the hood. */
         public static double getTargetHoodAngle() {
             return targetHoodAngle.get();
         }
 
         // Turret
+        /** Gets the kP for the turret's PID. */
         public static double getTurretKP() {
             return turretKP.get();
         }
 
+        /** Gets the kI for the turret's PID. */
         public static double getTurretKI() {
             return turretKI.get();
         }
 
+        /** Gets the kD for the turret's PID. */
         public static double getTurretKD() {
             return turretKD.get();
         }
 
+        /** Gets the target angle for the turret. */
         public static double getTargetTurretAngle() {
             return targetTurretAngle.get();
         };
         
         // Telemetry
+
+        /** Sends a value as the current turret angle. */
         public static void setTurretAngle(double angle) {
             turretAngle.set(angle);
         }
         
+        /** Sends a value as the current hood angle. */
         public static void setHoodAngle(double angle) {
             hoodAngle.set(angle);
         }
         
+        /** Sends a value as the current shooter speed. */
         public static void setTurretSpeed(double speed) {
             turretSpeed.set(speed);
         }
         
+        /** Sends a value as the current turret target. */
         public static void setTurretTarget(String target) {
             turretTarget.set(target);
         }
         
+        /** Sends a value as the current field zone. */
         public static void setDebugFieldZone(String zone) {
             debugFieldZone.set(zone);
         }
@@ -184,6 +207,9 @@ public class NetworkedConfig {
         private static final DoubleEntry spinnerSpeed = indexerTable
             .getDoubleTopic("Spinner Speed").getEntry(0);
         
+        /**
+         * Initializes the NetworkTables entries for the indexer subsystem.
+         */
         public static void initializeDefaults() {
             // Entries have defaults declared already, simply verify they are created on initialization
             spinnerKP.get();
@@ -194,27 +220,34 @@ public class NetworkedConfig {
         }
         
         // Spinner
+        
+        /** Gets the kP for the spindexer's PID. */
         public static double getSpinnerKP() {
             return spinnerKP.get();
         }
-        
+
+        /** Gets the kI for the spindexer's PID. */
         public static double getSpinnerKI() {
             return spinnerKI.get();
         }
-        
+
+        /** Gets the kD for the spindexer's PID. */
         public static double getSpinnerKD() {
             return spinnerKD.get();
         }
         
+        /** Gets the kV for the spindexer's Feed-Forward. */
         public static double getSpinnerKV() {
             return spinnerKV.get();
         }
         
+        /** Gets the target speed for the spindexer. */
         public static double getTargetSpeed() {
             return targetSpeed.get();
         }
         
         // Telemetry
+        /** Sends a value as the current spindexer speed. */
         public static void setSpinnerSpeed(double speed) {
             spinnerSpeed.set(speed);
         }
@@ -238,6 +271,9 @@ public class NetworkedConfig {
         private static final DoubleEntry intakeSpeed = intakeTable
             .getDoubleTopic("Intake Speed").getEntry(0);
         
+        /**
+         * Initializes the NetworkTables entries for the intake subsystem.
+         */
         public static void initializeDefaults() {
             // Entries have defaults declared already, simply verify they are created on initialization
             liftKP.get();
@@ -246,19 +282,24 @@ public class NetworkedConfig {
         }
         
         // Lift
+        
+        /** Gets the kP for the lift's PID. */
         public static double getLiftKP() {
             return liftKP.get();
         }
         
+        /** Gets the kI for the lift's PID. */
         public static double getLiftKI() {
             return liftKI.get();
         }
         
+        /** Gets the kD for the lift's PID. */
         public static double getLiftKD() {
             return liftKD.get();
         }
         
         // Telemetry
+        /** Sends a value as the current intake speed. */
         public static void setIntakeSpeed(double speed) {
             intakeSpeed.set(speed);
         }
