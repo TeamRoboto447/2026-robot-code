dc066d13a129106c6fbcdb2e0f4d7b41a9ae4578
Moonlit Productions <moonlit@munebase.dev>
2026-02-16 20:43:08 -0500

    Robot name decided, renamed robot class

--- src/main/java/frc/robot/Theseus.java
+++ src/main/java/frc/robot/Theseus.java
@@ -11,7 +11,7 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
-public class Robot extends TimedRobot {
+public class Theseus extends TimedRobot {
     private Command m_autonomousCommand;
 
     private final RobotContainer m_robotContainer;
@@ -21,7 +21,7 @@ public class Robot extends TimedRobot {
         .withTimestampReplay()
         .withJoystickReplay();
 
-    public Robot() {
+    public Theseus() {
         m_robotContainer = new RobotContainer();
         SignalLogger.enableAutoLogging(false);
     }
