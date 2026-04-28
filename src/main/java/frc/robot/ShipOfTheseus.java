c32b8f453f58329c46ef5166cfc830a1d1ee607a
darrint <darrint@fastmail.com>
2026-04-28 19:15:07 -0400

    Remove additional references to postEstimatorSubsystem

--- src/main/java/frc/robot/ShipOfTheseus.java
+++ src/main/java/frc/robot/ShipOfTheseus.java
@@ -333,16 +333,16 @@ public class ShipOfTheseus {
                 })
             );
 
-        DriverController.start().or(OperatorController.rightTrigger()).or(autoShootTrigger)
-            .onTrue(Commands.runOnce(() -> {
-                Alliance alliance = DriverStation.getAlliance().orElse(Alliance.Red);
-                poseEstimatorSubsystem.setTemporaryAprilTagFilter((alliance == Alliance.Red) ? Constants.FieldConstants.RED_HUB_APRILTAGS : Constants.FieldConstants.BLUE_HUB_APRILTAGS);
-                turretSubsystem.setShootingActive(true);
-            }))
-            .onFalse(Commands.runOnce(() -> {
-                poseEstimatorSubsystem.clearTemporaryAprilTagFilter();
-                turretSubsystem.setShootingActive(false);
-            }));
+        // DriverController.start().or(OperatorController.rightTrigger()).or(autoShootTrigger)
+        //     .onTrue(Commands.runOnce(() -> {
+        //         Alliance alliance = DriverStation.getAlliance().orElse(Alliance.Red);
+        //         poseEstimatorSubsystem.setTemporaryAprilTagFilter((alliance == Alliance.Red) ? Constants.FieldConstants.RED_HUB_APRILTAGS : Constants.FieldConstants.BLUE_HUB_APRILTAGS);
+        //         turretSubsystem.setShootingActive(true);
+        //     }))
+        //     .onFalse(Commands.runOnce(() -> {
+        //         poseEstimatorSubsystem.clearTemporaryAprilTagFilter();
+        //         turretSubsystem.setShootingActive(false);
+        //     }));
 
         DriverController.start().or(OperatorController.rightTrigger()).or(autoShootTrigger)
             .onFalse(Commands.runOnce(() -> {
@@ -712,13 +712,13 @@ public class ShipOfTheseus {
         );
 
         return Commands.sequence(
-            Commands.runOnce(() -> {
-                Alliance alliance = DriverStation.getAlliance().orElse(Alliance.Red);
-                Set<Integer> towerTags = (alliance == Alliance.Red) ? Set.of(15/*, 16*/) : Set.of(31/*, 32*/);
-                poseEstimatorSubsystem.setTemporaryAprilTagFilter(towerTags);
-            }),
+        //     Commands.runOnce(() -> {
+        //         Alliance alliance = DriverStation.getAlliance().orElse(Alliance.Red);
+        //         Set<Integer> towerTags = (alliance == Alliance.Red) ? Set.of(15/*, 16*/) : Set.of(31/*, 32*/);
+        //         poseEstimatorSubsystem.setTemporaryAprilTagFilter(towerTags);
+        //     }),
             climbSequence
-        ).finallyDo(interrupted -> poseEstimatorSubsystem.clearTemporaryAprilTagFilter());
+        );//.finallyDo(interrupted -> poseEstimatorSubsystem.clearTemporaryAprilTagFilter());
     }
 
     /**
