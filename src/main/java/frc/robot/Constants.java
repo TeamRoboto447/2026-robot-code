dc3468206e529a5061bef1eba4144ff266df7f8d
Ryno091510 <ryan.g.mejeur@gmail.com>
2026-07-11 13:58:54 -0400

    Added speed constraints while collecting, also changed lifter P to hold the intake better

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -497,7 +497,7 @@ public final class Constants {
 
         public static final double LIFT_GEARBOX_RATIO = 4.0*4*4;
 
-        public static final double LIFT_KP = 5;
+        public static final double LIFT_KP = 7;
         public static final double LIFT_KI = 0;
         public static final double LIFT_KD = 0.7;
 
